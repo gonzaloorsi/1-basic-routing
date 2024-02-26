387b98e982c0b48861825a771aabc9bbfb2c0f88
Gonzalo Orsi <gonzaloorsi@gmail.com>
2024-02-26 16:37:03 -0300

    routes created

---
+++ app/about/page.js
@@ -0,0 +1,5 @@
+function Page() {
+  return <div>Hello from about!</div>;
+}
+
+export default Page;
---
+++ app/contact/page.js
@@ -0,0 +1,5 @@
+function Page() {
+  return <div>Hello from contact!</div>;
+}
+
+export default Page;
